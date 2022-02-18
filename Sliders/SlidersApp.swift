739ab9805e90b4043c81df8d45cf5a760f3c5e53
Aleksandr Khasanov <sashokhasanov@mail.ru>
2022-02-18 14:57:02 +0300

    Initial Commit

---
+++ Sliders/SlidersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidersApp.swift
+//  Sliders
+//
+//  Created by Сашок on 18.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SlidersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
