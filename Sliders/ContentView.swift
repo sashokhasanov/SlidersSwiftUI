b6bae00e2989ae2d713ddb0b132cfdf027438912
Aleksandr Khasanov <sashokhasanov@mail.ru>
2022-02-22 19:59:48 +0300

    improved app functionality

--- Sliders/ContentView.swift
+++ Sliders/ContentView.swift
@@ -10,13 +10,10 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var redValue: Double = 125
-    @State private var redText: String = "125"
-    
     @State private var greenValue: Double = 125
-    @State private var greenText: String = "125"
-    
     @State private var blueValue: Double = 125
-    @State private var blueText: String = "125"
+    
+    @FocusState private var focusedField: Bool
     
     var body: some View {
         VStack {
@@ -27,9 +24,20 @@ struct ContentView: View {
                                        green: greenValue / 255,
                                        blue: blueValue / 255))
             
-            ColorComponentView(value: $redValue, text: $redText, color: .red)
-            ColorComponentView(value: $greenValue, text: $greenText, color: .green)
-            ColorComponentView(value: $blueValue, text: $blueText, color: .blue)
+            VStack {
+                ColorComponentView(value: $redValue, color: .red)
+                ColorComponentView(value: $greenValue, color: .green)
+                ColorComponentView(value: $blueValue, color: .blue)
+            }
+            .focused($focusedField)
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        focusedField = false
+                    }
+                }
+            }
             
             Spacer()
         }
@@ -39,19 +47,25 @@ struct ContentView: View {
 struct ColorComponentView: View {
     
     @Binding var value: Double
-    @Binding var text: String
+    @State var text: String = ""
     var color: Color
     
     var body: some View {
         HStack {
             Text(String(lround(value))).frame(width: 50, alignment: .leading)
+            
             Slider(value: $value, in: 0...255, step: 1)
                 .accentColor(color)
                 .onChange(of: self.value) { newValue in
                     text = String(lround(newValue))
                 }
+            
             ColorTextField(value: $value, text: $text)
-        }.padding()
+        }
+        .padding()
+        .onAppear {
+            text = String(lround(value))
+        }
     }
 }
 
@@ -63,37 +77,29 @@ struct ColorTextField: View {
     @State private var alertPresented: Bool = false
     
     var body: some View {
-        TextField("", text: $text)
-            .frame(width: 50)
-            .padding([.top, .leading, .bottom], 5.0)
-            .overlay(RoundedRectangle(cornerRadius: 5)
-                        .stroke(lineWidth: 3)
-                        .foregroundColor(.gray))
-            .keyboardType(.numbersAndPunctuation)
-            .onSubmit(of: .text, checkInput)
-            .alert("Wrong value", isPresented: $alertPresented, actions: {})
+        TextField("", text: $text) { _ in
+            withAnimation {
+                checkInput()
+            }
+        }
+        .frame(width: 50)
+        .padding([.top, .leading, .bottom], 5.0)
+        .overlay(RoundedRectangle(cornerRadius: 5)
+                    .stroke(lineWidth: 3)
+                    .foregroundColor(.gray))
+        .keyboardType(.decimalPad)
+        .onSubmit(of: .text, checkInput)
+        .alert("Wrong value", isPresented: $alertPresented, actions: {})
     }
     
     func checkInput() {
-        guard let value = Double(self.text) else {
+        guard let value = Int(self.text), (0...255).contains(value) else {
             alertPresented = true
             text = String(lround(value))
             return
         }
         
-        guard value >= 0 else {
-            alertPresented = true
-            self.value = 0
-            return
-        }
-        
-        guard value <= 255 else {
-            alertPresented = true
-            self.value = 255
-            return
-        }
-        
-        self.value = value
+        self.value = Double(value)
     }
 }
 
